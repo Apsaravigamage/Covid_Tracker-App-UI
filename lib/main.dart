59d59e5b73cf07fc43fc2468d21d1b648f7fec75
Apsaravi Gamage <apsaravigamage@gmail.com>
2021-01-07 20:05:48 +0530

    InitialCommit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:covid_tracker/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      fontFamily: 'Circular',
+    ),
+    home:HomePage(),
+  ));
+}
+
