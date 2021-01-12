4eddb76a995bb51c4398f5e1b7a68fad10d386a3
Apsaravi Gamage <apsaravigamage@gmail.com>
2021-01-12 19:51:09 +0530

    create country page

--- lib/pages/country.dart
+++ lib/pages/country.dart
@@ -16,14 +16,47 @@ class _CountryPageState extends State<CountryPage> {
     });
   }
   @override
+  void initState() {
+    fatchCountryData();
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:AppBar(
         title: Text('Country Stats'),
       ),
-        body: ListView.builder(
+        body: countryData==null?Center(child:CircularProgressIndicator(),):ListView.builder(
           itemBuilder: (context,index){
-            return Container();
+            return Container(
+              height: 130,
+              decoration: BoxDecoration(color: Colors.white,boxShadow: [
+                BoxShadow(color: Colors.grey[200],blurRadius: 10,offset: Offset(0,10)),
+              ]),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal:10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(countryData[index]['country'],style: TextStyle(fontWeight: FontWeight.bold),),
+                        Image.network(countryData[index]['countryInfo']['flag'],height: 50, width: 60,)
+                      ],),
+                  ),
+                  Expanded(child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children:<Widget>[
+                        Text('CONFIRMED'+ countryData[index]['cases'].toString(),style: TextStyle(fontWeight: FontWeight.bold,color: Colors.red,),),
+                        Text('ACTIVE'+ countryData[index]['active'].toString(),style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue,),),
+                        Text('RECOVERED'+ countryData[index]['cases'].toString(),style: TextStyle(fontWeight: FontWeight.bold,color: Colors.green,),),
+                        Text('DEATHS'+ countryData[index]['cases'].toString(),style: TextStyle(fontWeight: FontWeight.bold,color: Colors.grey[800],),),
+                      ]
+                  )
+                  ),)
+                ],),
+            );
           },  
      itemCount: countryData==null?0:countryData.length, 
     ),
